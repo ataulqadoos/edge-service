df7eecd6df85a0b3361aa4493122d1275018e1c7
Ataul Qadoos <ataulqadoos@gmail.com>
2024-01-18 08:08:55 -0500

    Added support for oauth2.

--- src/main/java/com/aqadoos/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/aqadoos/edgeservice/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
@@ -48,6 +50,11 @@ public class SecurityConfig {
             return chain.filter(exchange);
         };
 
+    }
+
+    @Bean
+    ServerOAuth2AuthorizedClientRepository auth2AuthorizedClientRepository() {
+        return new WebSessionServerOAuth2AuthorizedClientRepository();
     }
 
     private ServerLogoutSuccessHandler oidcLogoutSuccessHandler(
